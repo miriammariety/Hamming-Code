04bab9567b95cc4dc960e3560579ca41598415bc
Miriam Marie Ty <mcty@up.edu.ph>
2016-03-21 02:08:46 +0800

    Accepts valid inputs

--- src/hammingcode/HammingCode.java
+++ src/hammingcode/HammingCode.java
@@ -5,6 +5,8 @@
  */
 package hammingcode;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ty
@@ -16,6 +18,38 @@ public class HammingCode {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input:");
+        String input = in.nextLine();
+        while(invalidData(input)){
+            System.out.print("Invalid input! Input again: ");
+            input = in.nextLine();
+        }
+        
+        System.out.println("Select Parity: 1. Odd\t 2.Even");
+        int parity = in.nextInt();
+        while(invalidParity(parity)){
+            System.out.print("Invalid Parity! Input again: ");
+            parity = in.nextInt();
+        }
+        
+    }
+
+    private static boolean invalidData(String input) {
+        if(input.length()< 8 || input.length()>24)
+            return true;
+        for(int i=0; i<input.length(); i++){
+            if(input.charAt(i)!='1' && input.charAt(i)!='0')
+                return true;
+        }
+        
+        return false;
+    }
+
+    private static boolean invalidParity(int parity) {
+        if(parity > 2 || parity < 1)
+            return true;
+        return false;
     }
     
 }
