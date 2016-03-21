a27e030c165e04615340cd875adab6f6e8c0c917
Marie <mariethegreat89303@gmail.com>
2016-03-21 18:20:28 +0800

    finished part one of mp

--- src/hammingcode/HammingCode.java
+++ src/hammingcode/HammingCode.java
@@ -12,12 +12,17 @@ import java.util.Scanner;
  * @author ty
  */
 public class HammingCode {
-
+    int bitCount;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        HammingCode program = new HammingCode();
+        program.run();
+    }
+    
+    public void run(){
         Scanner in = new Scanner(System.in);
         System.out.println("Input:");
         String input = in.nextLine();
@@ -33,6 +38,7 @@ public class HammingCode {
             parity = in.nextInt();
         }
         
+        createCodeword(input, parity);
     }
 
     private static boolean invalidData(String input) {
@@ -51,5 +57,66 @@ public class HammingCode {
             return true;
         return false;
     }
-    
-}
+
+    private void createCodeword(String input, int parity) {
+        String temp = insertParity(input);
+        String codeword = replaceParity(temp, parity);
+        System.out.println("CODEWORD: " + codeword);
+    }
+
+    private String insertParity(String input) {
+        bitCount = (int) (Math.log(input.length()) / Math.log(2)) + 1;
+        int counter = 0;
+        StringBuilder builder = new StringBuilder(input);
+        while (counter < bitCount) {
+            builder.insert((int) Math.pow(2, counter++) - 1, 'X');
+        }
+        return builder.toString();
+    }
+
+    private String replaceParity(String temp, int parity) {
+        while(temp.contains("X")){
+            int start = temp.indexOf('X');
+            String bit = calculateCheckbit(temp, start, parity);
+            temp = temp.replaceFirst("X", bit);
+        }
+        return temp;
+    }
+
+    private String calculateCheckbit(String temp, int start, int parity) {
+        int skip = start + 1;
+        boolean use = true;
+        char[] bits = new char[bitCount * 2];
+        int j = 0;
+        for (int i = start, counter = 1; i < temp.length(); i++, counter++) {
+            if (use) {
+                bits[j++] = temp.charAt(i);
+            }
+            if (counter % skip == 0) {
+                use = !use;
+            }
+        }
+        int counter = 0;
+        for (char bit : bits) {
+            if (Character.isDigit(bit)) {
+                if (bit == '1') {
+                    counter++;
+                }
+            }
+        }
+        int p = 0;
+        switch (parity) {
+            case 2:
+                p = 0;
+                break;
+            case 1:
+                p = 1;
+                break;
+        }
+        if (counter % 2 == p) {
+            return "0";
+        } else {
+            return "1";
+        }
+    }
+    }
