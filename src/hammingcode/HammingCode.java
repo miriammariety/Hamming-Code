41b6fbb7b357d6a4dd7ace341aae4d8e9955608a
Marie <mariethegreat89303@gmail.com>
2016-03-27 04:19:16 +0800

    Added implementation of correcting codeword

--- src/hammingcode/HammingCode.java
+++ src/hammingcode/HammingCode.java
@@ -12,7 +12,9 @@ import java.util.Scanner;
  * @author ty
  */
 public class HammingCode {
+
     int bitCount;
+
     /**
      * @param args the command line arguments
      */
@@ -20,52 +22,78 @@ public class HammingCode {
         // TODO code application logic here
         HammingCode program = new HammingCode();
         Scanner in = new Scanner(System.in);
-        System.out.print("Choose: 1. Create Hamming Code\t 2.Correct Hamming Code");
-        int choice = in.nextInt();
-        while(invalidChoice(choice)){
-            System.out.println("Invalid choice! Input again: ");
-            choice = in.nextInt();
+        boolean runProgram = true;
+        while (runProgram) {
+            System.out.println("Choose: 1. Create Hamming Code\t 2.Correct Hamming Code");
+            int choice = in.nextInt();
+            while (invalidChoice(choice)) {
+                System.out.println("Invalid choice! Input again: ");
+                choice = in.nextInt();
+            }
+            if (choice == 1) {
+                program.create();
+            } else if (choice == 2) {
+                program.correct();
+            }
+            in.nextLine();
+            System.out.println("Run again? y/n");
+            String answer = in.nextLine();
+            answer = answer.toLowerCase();
+            runProgram = answer.equals("y");
         }
-        if(choice==1)
-            program.create();
-        else if (choice==2)
-            program.correct();
+
     }
-    
-    public void create(){
+
+    public void create() {
         Scanner in = new Scanner(System.in);
         System.out.println("Input data:");
         String input = in.nextLine();
-        while(invalidData(input)){
+        while (invalidData(input)) {
             System.out.print("Invalid input! Input again: ");
             input = in.nextLine();
         }
-        
+
         System.out.println("Select Parity: 1. Odd\t 2.Even");
         int parity = in.nextInt();
-        while(invalidChoice(parity)){
+        while (invalidChoice(parity)) {
             System.out.print("Invalid Parity! Input again: ");
             parity = in.nextInt();
         }
-        
+
         createCodeword(input, parity);
     }
 
-    private static boolean invalidData(String input) {
-        if(input.length()< 8 || input.length()>24)
-            return true;
-        for(int i=0; i<input.length(); i++){
-            if(input.charAt(i)!='1' && input.charAt(i)!='0')
-                return true;
-        }
-        
-        return false;
-    }
+    public void correct() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input Codeword: ");
+        String input = in.nextLine();
 
-    private static boolean invalidChoice(int choice) {
-        if(choice > 2 || choice < 1)
-            return true;
-        return false;
+        //Ask for codeword
+        while (invalidData(input)) {
+            System.out.print("Invalid input! Input again: ");
+            input = in.nextLine();
+        }
+
+        //Ask for parity
+        System.out.println("Select Parity: 1. Odd\t 2.Even");
+        int parity = in.nextInt();
+        while (invalidChoice(parity)) {
+            System.out.print("Invalid Parity! Input again: ");
+            parity = in.nextInt();
+        }
+
+        //Correct codeword
+        int bitError = checkBitError(input, parity);
+        if (bitError != 0) {
+            System.out.println("Codeword (Corrected): ");
+            StringBuilder sb = new StringBuilder(input);
+            if (input.charAt(bitError - 1) == '0') {
+                sb.setCharAt((bitError - 1), '1');
+            } else {
+                sb.setCharAt((bitError - 1), '0');
+            }
+            System.out.println(sb);
+        }
     }
 
     private void createCodeword(String input, int parity) {
@@ -85,7 +113,7 @@ public class HammingCode {
     }
 
     private String replaceParity(String temp, int parity) {
-        while(temp.contains("X")){
+        while (temp.contains("X")) {
             int start = temp.indexOf('X');
             String bit = calculateCheckbit(temp, start, parity);
             temp = temp.replaceFirst("X", bit);
@@ -114,6 +142,70 @@ public class HammingCode {
                 }
             }
         }
+
+        return setParity(parity, counter);
+    }
+
+    private int checkBitError(String input, int parity) {
+        bitCount = (int) (Math.log(input.length()) / Math.log(2)) + 1;
+        int position = 0;
+        for (int i = 0; i < bitCount; i++) {
+            int rIndex = (int) Math.pow(2, i) - 1;
+            if (calculateR(input, rIndex, parity).equals("1")) {
+                position += rIndex + 1;
+            }
+        }
+
+        if (position == 0) {
+            System.out.println("No errors");
+        } else {
+            System.out.println("Error at position " + position);
+        }
+
+        return position;
+    }
+
+    private String calculateR(String input, int rIndex, int parity) {
+        int skip = rIndex + 1;
+        boolean isPart = true;
+        int bitCounter = 0;
+        for (int i = rIndex, ctr = 1; i < input.length(); i++, ctr++) {
+            if (isPart) {
+
+                if (input.charAt(i) == '1') {
+                    bitCounter++;
+                }
+            }
+
+            if (ctr % skip == 0) {
+                isPart = !isPart;
+            }
+        }
+
+        return setParity(parity, bitCounter);
+    }
+
+    private static boolean invalidData(String input) {
+        if (input.length() < 8 || input.length() > 24) {
+            return true;
+        }
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != '1' && input.charAt(i) != '0') {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean invalidChoice(int choice) {
+        if (choice > 2 || choice < 1) {
+            return true;
+        }
+        return false;
+    }
+
+    private String setParity(int parity, int counter) {
         int p = 0;
         switch (parity) {
             case 2:
@@ -123,45 +215,11 @@ public class HammingCode {
                 p = 1;
                 break;
         }
+
         if (counter % 2 == p) {
             return "0";
         } else {
             return "1";
         }
-    }
-
-    public void correct() {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Input Codeword:");
-        String input = in.nextLine();
-        while(invalidData(input)){
-            System.out.print("Invalid input! Input again: ");
-            input = in.nextLine();
-        }
-        
-        System.out.println("Select Parity: 1. Odd\t 2.Even");
-        int parity = in.nextInt();
-        while(invalidChoice(parity)){
-            System.out.print("Invalid Parity! Input again: ");
-            parity = in.nextInt();
-        }
-        
-        int bitError = checkBitError(input, parity);
-    }
-
-    private int checkBitError(String input, int parity) {
-        bitCount = (int) (Math.log(input.length()) / Math.log(2)) + 1;
-        int position = 0;
-        for(int i=0; i < bitCount; i++){
-            int rIndex = (int) Math.pow(2,i) -1;
-            if(calculateR(input, rIndex, parity).equals("1")){
-                position += rIndex+1;
-            }
-        }
-        
-        if(position==0)
-            System.out.println("No errors");
-        else
-            System.out.println("Error at position " + position);
     }
 }
